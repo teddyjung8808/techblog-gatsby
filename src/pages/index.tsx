fea2034d0c98939dfa2338ee607a6921c2dd115f
teddyjung8808 <teddyjung8808@gmail.com>
2022-08-26 16:58:58 +0900

    add: 타입스크립트, eslint & prettier 설정

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react'
+import Text from '@Components/Text'
+
+const IndexPage: FunctionComponent = function () {
+  return <Text text="Home" />
+}
+
+export default IndexPage
